f57f303c1cc74c40c169b515a380275eb4659050
whiteWasp <djkolesa@gmail.com>
2019-06-23 15:23:52 -0400

    David Kolesar is using ES6 method to call GET requests.

--- ticker.js
+++ ticker.js
@@ -1,26 +1,23 @@
+//ES6 method to to GET requests
 function activateTicker() {
-	//define callback	
-	function callback(retVal){
- 		console.log(retVal);
-    }
-
 	var url = "https://api.alternative.me/v2/ticker/?convert=EUR";
-	
 	fetch(url).then(function(response) {
 	  return response.json();
 	}).then(function(data) {
 	  console.log(data);
 	}).catch(function() {
-	  console.log("Booo");
+	  console.log("Unable to retrieve ticker data -- ticker server is down.");
 	});
-	
 }
 
 
+//define callback for old GET request method
+function callback(retVal){
+ 	console.log(retVal);
+}
 
 
-
-
+// old school way to perform GET request in JS
 function queryTicker(callback)
 {	
 	console.log('connected to ticker');
