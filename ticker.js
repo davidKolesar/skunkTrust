4b4c973d0b6697c520965e53e0585ff5cb11adef
whiteWasp <djkolesa@gmail.com>
2019-06-23 15:44:53 -0400

    David Kolesar is fighting CORS

--- ticker.js
+++ ticker.js
@@ -1,37 +1,30 @@
 //ES6 method to to GET requests
 function activateTicker() {
-	var url = "https://api.alternative.me/v2/ticker/?convert=EUR";
-	var http_request;
-http_request = new XMLHTTPRequest();
-http_request.onreadystatechange = function () { /* .. */ };
-http_request.open("GET", url);
-http_request.withCredentials = true;
-http_request.setRequestHeader("Content-Type", "application/json");
-var response = http_request.send({ 'request': "authentication token" });
-
-	console.log(JSON.parse(response));
+queryTicker();
 }
 
 
 //define callback for old GET request method
 function callback(retVal){
- 	console.log(retVal);
+ 	console.log(JSON.parse(retVal));
 }
 
 
 // old school way to perform GET request in JS
 function queryTicker(callback)
 {	
-	console.log('connected to ticker');
 	var tickerEndpoint = "https://api.alternative.me/v2/ticker/?convert=EUR"
-    var jsonResponse;
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          jsonResponse =  callback(xmlHttp.responseText);
-			}
-    xmlHttp.open("GET", tickerEndpoint, true); // boolean for asynchronous 
-    xmlHttp.send(null);
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let response = JSON.parse(this.responseText);
+      callback(response);    }
+  }
+  xhr.open("GET", tickerEndpoint);
+  xhr.setRequestHeader("Accept", 'application/json');
+  xhr.send();
+}
 }
 
 //endpoints to consider: https://api.cryptowat.ch/markets/summaries
+
