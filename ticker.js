006aab1e626654d55375d639146f8ffe9b7e129a
whiteWasp <djkolesa@gmail.com>
2019-06-23 16:22:06 -0400

    David Kolesar has fixed CORS errors / is trying to modify data to return to DOM element

--- ticker.js
+++ ticker.js
@@ -7,20 +7,25 @@ queryTicker();
 
 
 // pre ES6 way to perform GET request in JS
-function queryTicker()
+function queryTicker(callback)
 {	
 	var tickerEndpoint = "https://api.alternative.me/v2/ticker/?convert=EUR"
 	let xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
+
+	//define callback to handle async call
+	function callback(response){
+		console.log("executing callback");
+ 		console.log(JSON.parse(response));
+	}
+
+	
+	xhr.onreadystatechange = function(callback) {
 		if (this.readyState === 4 && this.status === 200) {
 		  var response = JSON.parse(this.responseText);
-			console.log(response);
+			callback(response);
 		}
-			return response;
 	  	}
 	xhr.open("GET", "https://cors-anywhere.herokuapp.com/" + tickerEndpoint);
 	xhr.setRequestHeader("Accept", 'application/json');
 	tickerData = xhr.send();
- 
 }
-
