f9f9741283aaec2f0df7a638a4a7cba972198b1a
whiteWasp <djkolesa@gmail.com>
2019-06-23 15:49:31 -0400

    David Kolesar is fighting CORS

--- ticker.js
+++ ticker.js
@@ -14,15 +14,15 @@ function callback(retVal){
 function queryTicker(callback)
 {	
 	var tickerEndpoint = "https://api.alternative.me/v2/ticker/?convert=EUR"
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      let response = JSON.parse(this.responseText);
-      callback(response);    }
-  }
-  xhr.open("GET", tickerEndpoint);
-  xhr.setRequestHeader("Accept", 'application/json');
-  xhr.send();
+	let xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+		  let response = JSON.parse(this.responseText);
+		  callback(response);    }
+	  	}
+	xhr.open("GET", "https://cors-anywhere.herokuapp.com/" + tickerEndpoint);
+	xhr.setRequestHeader("Accept", 'application/json');
+    xhr.send();
 }
 
 //endpoints to consider: https://api.cryptowat.ch/markets/summaries
