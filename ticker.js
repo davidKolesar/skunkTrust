c24c44e96d3e403ac9b8f307d7e7e3d99704a499
whiteWasp <djkolesa@gmail.com>
2019-06-23 16:31:18 -0400

    David Kolesar is trying to modify data to return to DOM element

--- ticker.js
+++ ticker.js
@@ -13,11 +13,11 @@ function queryTicker(callback)
 	
 	xhr.onreadystatechange = function(callback) {
 		if (this.readyState === 4 && this.status === 200) {
-		  var response = this.responseText;
-		  return response;
+		  var response = JSON.parse(this.responseText);
+			if(response !== undefined) {
+				tickerData = response;	
+			}
 		}
-		tickerData = response;
-		console.log(JSON.parse(tickerData));
 	}
 	xhr.open("GET", "https://cors-anywhere.herokuapp.com/" + tickerEndpoint);
 	xhr.setRequestHeader("Accept", 'application/json');
