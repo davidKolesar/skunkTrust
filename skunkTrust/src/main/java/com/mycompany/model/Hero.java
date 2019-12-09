ee3417bf144176d71887d46b2291b13f2dd918d9
unknown <djkolesa@gmail.com>
2019-12-08 17:13:26 -0800

    David Kolesar is working on game directories

--- skunkTrust/src/main/java/com/mycompany/model/Hero.java
+++ skunkTrust/src/main/java/com/mycompany/model/Hero.java
@@ -8,7 +8,27 @@ public class Hero {
 	private int hitPoints;
 	private ArrayList<Item> items;
 	
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+	public ArrayList<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
 	
+	public String getName() {
+		return name;
+	}
 	
-	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
