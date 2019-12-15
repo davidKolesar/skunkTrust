e75832d864e018f68c832ae1b3d57c20b2e822dd
unknown <djkolesa@gmail.com>
2019-12-14 21:31:47 -0800

    Moving logic to .js

--- js/skunkTrust.js
+++ js/skunkTrust.js
@@ -1,13 +1,10 @@
-//allow textbox to submit with return button (helps with other browsers)
-var input = document.getElementById("terminalReturn");
+var input = document.getElementById("input");
 input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("terminalReturn").click();
-  }
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		document.getElementById("terminalReturn").click();
+	}
 });
-
-
 
 function sendJSON() {
 	alert("goodbye, world.");
