0fc16e8cf5bb465b593ae415712c83fb1c998a7b
unknown <djkolesa@gmail.com>
2019-12-14 21:17:53 -0800

    Working on button

--- js/skunkTrust.js
+++ js/skunkTrust.js
@@ -1,5 +1,19 @@
+//setting button invisible
+document.getElementById("terminalReturn").style.visibility="hidden";
+
+//allow textbox to submit with return button (helps with other browsers)
+var input = document.getElementById("terminalReturn");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("terminalReturn").click();
+  }
+});
+
+
+
 function sendJSON() {
-	console.log("goodbye, world.");
+	alert("goodbye, world.");
 }
 
 
