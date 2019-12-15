3122c1f5435a531e73bcbbfe5ca20ff18f07ef6f
unknown <djkolesa@gmail.com>
2019-12-14 22:11:21 -0800

    David Kolesar is adding validation

--- js/skunkTrust.js
+++ js/skunkTrust.js
@@ -1,7 +1,62 @@
-
-function sendJSON() {
+function validateInput() {
 	var input = document.getElementById("input");
-	alert(input);
+	
+	/*
+	*Checks if values are "truthy." This includes checks for:
+	* - null
+	* - undefined
+	* - NaN
+	* - empty string ("")
+	* - 0
+	* - false
+	*/
+	if(input){
+		sendJSON(input);
+	}
+
+function sendJSON(input) {
+	
+	/*
+	* 1. Take input from terminalReturn
+	* 1.5 client-side validation is performed
+	* 2. Serialize it as JSON
+	* 3. Send it to endpoint (serverside)
+	* 4. Input gets sent to console IO (server-side validation)
+	* 5. Gameplay business logic executed
+	* 6. Response it output to terminal
+	* 7. Terminal is refreshed
+	*/
+	
+	
+	        var restEndpoint
+            let result = document.querySelector('.result'); 
+            let name = document.querySelector('#name'); 
+            let email = document.querySelector('#email'); 
+               
+            // Creating a XHR object 
+            let xhr = new XMLHttpRequest(); 
+        
+            // open a connection 
+            xhr.open("POST", restEndpoint, true); 
+  
+            // Set the request header i.e. which type of content you are sending 
+            xhr.setRequestHeader("Content-Type", "application/json"); 
+  
+            // Create a state change callback 
+            xhr.onreadystatechange = function () { 
+                if (xhr.readyState === 4 && xhr.status === 200) { 
+  
+                    // Print received data from server 
+                    result.innerHTML = this.responseText; 
+  
+                } 
+            }; 
+  
+            // Converting JSON data to string 
+            var data = JSON.stringify({ "name": name.value, "email": email.value }); 
+  
+            // Sending data with the request 
+            xhr.send(data); 
 }
 
 
