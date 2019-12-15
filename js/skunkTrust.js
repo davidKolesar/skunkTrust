f51d3d16fc67309592fc9c047bbd435e3ee08128
unknown <djkolesa@gmail.com>
2019-12-14 21:48:34 -0800

    David Kolesar is working on boilerplate to serialize input as JSON to backend

--- js/skunkTrust.js
+++ js/skunkTrust.js
@@ -1,13 +1,7 @@
-var input = document.getElementById("input");
-input.addEventListener("keyup", function(event) {
-	if (event.keyCode === 13) {
-		event.preventDefault();
-		document.getElementById("terminalReturn").click();
-	}
-});
 
 function sendJSON() {
-	alert("goodbye, world.");
+	var input = document.getElementById("input");
+	alert(input);
 }
 
 
