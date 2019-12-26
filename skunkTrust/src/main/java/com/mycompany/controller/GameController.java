63bc027dbf9065b5af130dc389ec13016fce7b9a
unknown <djkolesa@gmail.com>
2019-12-25 18:57:28 -0800

    Working on connecting front end and back end <3

--- skunkTrust/src/main/java/com/mycompany/controller/GameController.java
+++ skunkTrust/src/main/java/com/mycompany/controller/GameController.java
@@ -1,16 +1,16 @@
 package com.mycompany.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/kolesar")
+@RequestMapping("/skunktrust")
 public class GameController {
 
-  @GetMapping("/hello-world")
-  public String sayHello(String name) {
-    return name;
+  @PostMapping("/gameController")
+  public String takeInput(String input) {
+    return "Hello front-end!";
   }
 
   //post request here is what was sent from terminal
