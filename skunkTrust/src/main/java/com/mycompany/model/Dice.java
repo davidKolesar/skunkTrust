df58b1b1a2fa14626ddaab68e0b508031a65d4b8
djkolesa@gmail.com <djkolesa@gmail.com>
2020-04-18 12:46:46 -0400

    David Kolesar added dice

---
+++ skunkTrust/src/main/java/com/mycompany/model/Dice.java
@@ -0,0 +1,21 @@
+package com.mycompany.model;
+
+import java.util.Random;
+
+public class Dice {
+	Random r = new Random();
+	private int low;
+	private int high;
+	private int result;
+	
+	public Dice(int low, int high) {
+	this.low = low;
+	this.high = high;
+	this.result = r.nextInt(high-low)+low;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+}
